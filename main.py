3af1e31aab012e246956eb6c3758f108dda0e1c8
Laura Porsch <laupor112@gmail.com>
2023-04-01 11:32:01 -0600

    Second Version, cleaned up code, no restart question after false character, morse code dictionary in seperate file

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
-MORSE_CODE = {"A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.", "G": "--.", "H": "....",
-                   "I": "..", "J": ".---", "K": "-.-", "L": ".-..", "M": "--", "N": "-.", "O": "---", "P": ".--.",
-                   "Q": "--.-", "R": ".-.", "S": "...", "T": "-", "U": "..-", "V": "...-", "W": ".--", "X": "-..-",
-                   "Y": "-.--", "Z": "--..", "1": ".----", "2": "..---", "3": "...--", "4": "....-", "5": ".....",
-                   "6": "-....", "7": "--...", "8": "---..", "9": "----.", "0": "-----", ", ": "--..--", ".": ".-.-.-",
-                   "?": "..--..", " ": "//"}
+from MORSE_CODE import MORSE_CODE
 
 
 def convert_to_morse(message):
     code = ""
     for letter in message:
         if letter not in MORSE_CODE:
-            print("Sorry, you used a sign that is not used in Morse Code, please try again.")
-            return
+            print("Sorry, you used a character that is not used in Morse Code, please try again.")
+            return True
         else:
             code += MORSE_CODE[letter] + "  "
     print(f"Whitespace separates single characters, // equals whitespace and separates words\n "
           f"This is your encrypted message:\n"
           f"{code}")
-
+    restart = input("Would you like to convert more messages? Type Yes or No  ").lower()
+    if restart == "yes":
+        return True
+    else:
+        print("Thank you for using the Morse Code Converter!")
+        return False
 
 print("███    ███  ██████  ██████  ███████ ███████      ██████  ██████  ██████  ███████\n"
       "████  ████ ██    ██ ██   ██ ██      ██          ██      ██    ██ ██   ██ ██\n"
       "██ ████ ██ ██    ██ ██████  ███████ █████       ██      ██    ██ ██   ██ █████\n"
       "██  ██  ██ ██    ██ ██   ██      ██ ██          ██      ██    ██ ██   ██ ██\n"
       "██      ██  ██████  ██   ██ ███████ ███████      ██████  ██████  ██████  ███████")
-# User prompt
+
 print("Hello, welcome to the Morse Code Converter.\n"
       "This program will turn your written message into Morse Code.\n"
       "You can use letters, numbers, commas, periods and question marks.\n")
-continue_convert = True
-while continue_convert:
+more_messages = True
+
+while more_messages:
     user_text = input("Please enter the text you would like to encode to Morse Code.\n").upper()
-    convert_to_morse(message=user_text)
-    more_messages = input("Would you like to convert more messages? Type Yes or No  ").lower()
-    if more_messages == "yes":
-        pass
-    else:
-        continue_convert = False
-        print("Thank you for using the Morse Code Converter!")
+    more_messages = convert_to_morse(message=user_text)
+
 
